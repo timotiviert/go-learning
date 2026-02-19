d607c00aa7894185617c5e38ded70cccc5ad1923
timotiviert <hofmann.ti@t-online.de>
2026-02-19 18:11:21 +0100

    Add map-exercise

---
+++ 01-basics/map-exercise/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	count := make(map[string]int)
+	words := strings.Fields(s)
+
+	for _, word := range words {
+		elem, ok := count[word]
+
+		if ok == true {
+			count[word] = elem + 1
+		} else {
+			count[word] = 1
+		}
+	}
+	return count
+}
+
+func main() {
+	wc.Test(WordCount)
+}
