e53d54c727b95e6de6ebbca16379a744ee0a3740
timotiviert <hofmann.ti@t-online.de>
2026-02-25 19:30:54 +0100

    Improve FileStorage

--- 01-basics/storage-interface/main.go
+++ 01-basics/storage-interface/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"encoding/csv"
+	"errors"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
+	"runtime"
 )
 
 type Storage interface {
@@ -12,62 +15,113 @@ type Storage interface {
 }
 
 type FileStorage struct {
-	Path string
+	Dir string
 }
 
-func NewFileStorage(path string) (*FileStorage, error) {
-	header := []string{"key", "value"}
+func NewFileStorage() (*FileStorage, error) {
+	// Return absolute path to executed Go file
+	_, filename, _, ok := runtime.Caller(0)
 
-	// Get file stats.
-	info, err := os.Stat(path)
-	var isNewFile bool
-	if os.IsNotExist(err) {
-		isNewFile = true
-	} else if err != nil {
-		return nil, err
-	} else {
-		isNewFile = info.Size() == 0
+	if !ok {
+		return nil, errors.New("could not get filename")
 	}
 
-	if isNewFile {
-		f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			return nil, err
-		}
-		defer f.Close()
+	dir := filepath.Join(filepath.Dir(filename), "files")
 
-		writer := csv.NewWriter(f)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return nil, err
 
-		if err := writer.Write(header); err != nil {
-			return nil, err
-		}
-		writer.Flush()
-		if err := writer.Error(); err != nil {
-			return nil, err
-		}
 	}
 
 	return &FileStorage{
-		Path: path,
+		Dir: dir,
 	}, nil
+
+	//header := []string{"key", "value"}
+	//
+	//// Get file stats.
+	//info, err := os.Stat(path)
+	//var isNewFile bool
+	//if os.IsNotExist(err) {
+	//	isNewFile = true
+	//} else if err != nil {
+	//	return nil, err
+	//} else {
+	//	isNewFile = info.Size() == 0
+	//}
+	//
+	//if isNewFile {
+	//	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//	defer f.Close()
+	//
+	//	writer := csv.NewWriter(f)
+	//
+	//	if err := writer.Write(header); err != nil {
+	//		return nil, err
+	//	}
+	//	writer.Flush()
+	//	if err := writer.Error(); err != nil {
+	//		return nil, err
+	//	}
+	//}
+	//
+	//return &FileStorage{
+	//	Path: path,
+	//}, nil
 }
 
 func (fs *FileStorage) Save(key, value string) error {
-	f, err := os.OpenFile(fs.Path, os.O_APPEND|os.O_WRONLY, 0644)
+	path := filepath.Join(fs.Dir, key)
+	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
-
-	writer := csv.NewWriter(f)
-
-	if err := writer.Write([]string{key, value}); err != nil {
-		return err
-	}
-	writer.Flush()
-	return writer.Error()
+	fmt.Fprintln(f, value)
+	return nil
+	//f, err := os.OpenFile(fs.Path, os.O_APPEND|os.O_WRONLY, 0644)
+	//if err != nil {
+	//	return err
+	//}
+	//defer f.Close()
+	//
+	//writer := csv.NewWriter(f)
+	//
+	//if err := writer.Write([]string{key, value}); err != nil {
+	//	return err
+	//}
+	//writer.Flush()
+	//return writer.Error()
 }
 
 func (fs *FileStorage) Load(key string) (string, error) {
+	path := filepath.Join(fs.Dir, key)
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
+
+func main() {
+	var fs Storage
+
+	fs, err := NewFileStorage()
+	if err != nil {
+		log.Fatal()
+	}
+
+	if err := fs.Save("key", "hi"); err != nil {
+		log.Fatal(err)
+	}
+
+	value, err := fs.Load("key")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(value)
 
 }
