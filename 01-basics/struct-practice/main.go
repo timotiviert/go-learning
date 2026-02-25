8439b3918653a318875d6a852c921c8ec9eedfa0
timotiviert <hofmann.ti@t-online.de>
2026-02-25 14:23:33 +0100

    Add JSON Marshalling and Unmarshalling for User struct

--- 01-basics/struct-practice/main.go
+++ 01-basics/struct-practice/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"math/rand"
 	"net/mail"
 )
 
 type User struct {
-	Id    int
-	Name  string
-	Email string
-	Age   int
+	Id    int    `json:"id"`
+	Name  string `json:"username"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
+
+func New(name string, email string, age int) *User {
+	_, err := mail.ParseAddress(email)
+	if err != nil {
+		return nil
+	}
+	return &User{
+		Id:    rand.Intn(100),
+		Name:  name,
+		Email: email,
+		Age:   age,
+	}
 }
 
 func (u User) IsAdult() bool {
@@ -28,14 +44,34 @@ func (u *User) UpdateEmail(email string) error {
 	return nil
 }
 
-func New(name, email string, ege int) (*User, error) {
-	_, err := mail.ParseAddress(email)
+func UserToJSON(u *User) ([]byte, error) {
+	jsonData, err := json.Marshal(u)
 	if err != nil {
 		return nil, err
 	}
-	return &User{
-		Id:    rand.Intn(100),
-		Name:  name,
-		Email: email,
-	}, nil
+	return jsonData, nil
+}
+
+func NewFromJSON(jsonData []byte) (*User, error) {
+	var u User
+	err := json.Unmarshal(jsonData, &u)
+	if err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
+
+func main() {
+	gandalf := New("Gandalf the Grey", "mithrandir@wizzard.com", 2000)
+	gandalfJSON, err := UserToJSON(gandalf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(gandalfJSON))
+	frodoJSON := `{"username": "Frodo", "email": "frodo.baggins@shire.com", "age": 18}`
+	frodo, err := NewFromJSON([]byte(frodoJSON))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(*frodo)
 }
