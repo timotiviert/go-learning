6b799cef4a5905674a7656fd95bcf42d29443dc5
timotiviert <hofmann.ti@t-online.de>
2026-02-26 12:15:06 +0100

    Remove redundant code

--- 01-basics/storage-interface/main.go
+++ 01-basics/storage-interface/main.go
@@ -36,41 +36,6 @@ func NewFileStorage() (*FileStorage, error) {
 	return &FileStorage{
 		Dir: dir,
 	}, nil
-
-	//header := []string{"key", "value"}
-	//
-	//// Get file stats.
-	//info, err := os.Stat(path)
-	//var isNewFile bool
-	//if os.IsNotExist(err) {
-	//	isNewFile = true
-	//} else if err != nil {
-	//	return nil, err
-	//} else {
-	//	isNewFile = info.Size() == 0
-	//}
-	//
-	//if isNewFile {
-	//	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	//	if err != nil {
-	//		return nil, err
-	//	}
-	//	defer f.Close()
-	//
-	//	writer := csv.NewWriter(f)
-	//
-	//	if err := writer.Write(header); err != nil {
-	//		return nil, err
-	//	}
-	//	writer.Flush()
-	//	if err := writer.Error(); err != nil {
-	//		return nil, err
-	//	}
-	//}
-	//
-	//return &FileStorage{
-	//	Path: path,
-	//}, nil
 }
 
 func (fs *FileStorage) Save(key, value string) error {
@@ -82,19 +47,6 @@ func (fs *FileStorage) Save(key, value string) error {
 	defer f.Close()
 	fmt.Fprintln(f, value)
 	return nil
-	//f, err := os.OpenFile(fs.Path, os.O_APPEND|os.O_WRONLY, 0644)
-	//if err != nil {
-	//	return err
-	//}
-	//defer f.Close()
-	//
-	//writer := csv.NewWriter(f)
-	//
-	//if err := writer.Write([]string{key, value}); err != nil {
-	//	return err
-	//}
-	//writer.Flush()
-	//return writer.Error()
 }
 
 func (fs *FileStorage) Load(key string) (string, error) {
