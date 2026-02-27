06a428f960e1b3e0bb2d7d5642292e10c00ad3a7
timotiviert <hofmann.ti@t-online.de>
2026-02-27 17:34:01 +0100

    Make Crawl parallel working with Mutex, WaitGroup, Recursion and closure :)

--- 02-concurrency/web-crawler/README.md
+++ 02-concurrency/web-crawler/README.md
@@ -5,3 +5,5 @@ In this exercise you'll use Go's concurrency features to parallelize a web crawl
 Modify the `Crawl` function to fetch URLs in parallel without fetching the same URL twice.
 
 _Hint_: you can keep a cache of the URLs that have been fetched on a map, but maps alone are not safe for concurrent use!
+
+Source: https://go.dev/tour/concurrency/10
--- 02-concurrency/web-crawler/main.go
+++ 02-concurrency/web-crawler/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 type Fetcher interface {
@@ -13,21 +14,55 @@ type Fetcher interface {
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
 func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: Fetch URLs in parallel.
-	// TODO: Don't fetch the same URL twice.
-	// This implementation doesn't do either:
-	if depth <= 0 {
-		return
+	// As this function is a closure, the crawl function can access the local variables of the Crawl function.
+	// Mutex and WaitGROup to synchronize access to fetched.
+	var mu sync.Mutex
+	var wg sync.WaitGroup
+
+	fetched := make(map[string]bool)
+
+	var crawl func(string, int)
+	crawl = func(url string, depth int) {
+		defer wg.Done()
+
+		// Max depth reached.
+		if depth <= 0 {
+			return
+		}
+
+		// Lock Mutex and check if url already fetched.
+		mu.Lock()
+		if fetched[url] {
+			// URL already fetched, release lock and return.
+			mu.Unlock()
+			return
+		}
+		// Remember URL as fetched and release lock.
+		fetched[url] = true
+		mu.Unlock()
+
+		// Fetch content from URL.
+		body, urls, err := fetcher.Fetch(url)
+		if err != nil {
+			// Could not fetch, so set URL to false in fetched, another worker might try again.
+			mu.Lock()
+			fetched[url] = false
+			mu.Unlock()
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("found: %s %q\n", url, body)
+
+		// Recursively crawl URLs one level deeper.
+		for _, u := range urls {
+			wg.Add(1)
+			go crawl(u, depth-1)
+		}
 	}
-	body, urls, err := fetcher.Fetch(url)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("found: %s %q\n", url, body)
-	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
-	}
+
+	wg.Add(1)
+	go crawl(url, depth)
+	wg.Wait()
 	return
 }
 
