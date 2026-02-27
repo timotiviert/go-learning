4194982023fefbc001a85f9bb537bbed67a13f12
timotiviert <hofmann.ti@t-online.de>
2026-02-27 17:36:29 +0100

    Add CrawlOld() for comparison

--- 02-concurrency/web-crawler/main.go
+++ 02-concurrency/web-crawler/main.go
@@ -66,6 +66,27 @@ func Crawl(url string, depth int, fetcher Fetcher) {
 	return
 }
 
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func CrawlOld(url string, depth int, fetcher Fetcher) {
+	// TODO: Fetch URLs in parallel.
+	// TODO: Don't fetch the same URL twice.
+	// This implementation doesn't do either:
+	if depth <= 0 {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		Crawl(u, depth-1, fetcher)
+	}
+	return
+}
+
 func main() {
 	Crawl("https://golang.org/", 4, fetcher)
 }
