160637889b1bf015b04177efe73238ed6a3b13a9
timotiviert <hofmann.ti@t-online.de>
2026-03-02 18:41:40 +0100

    Add ping example from https://gin-gonic.com/en/docs/

---
+++ 02-concurrency/worker-pool/README.md
@@ -0,0 +1,8 @@
+# Worker Pool
+
+Implementiere einen Worker Pool:
+- 5 Worker Goroutines
+- Job Queue (buffered channel)
+- Result Channel
+- Graceful Shutdown
+- Jobs: Simuliere API Calls mit time.Sleep
---
+++ 03-rest-api/cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	if err := r.Run(); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
