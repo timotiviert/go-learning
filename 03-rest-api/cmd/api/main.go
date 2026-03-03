4aa5e7a1196f513e5153e7cff865963002b1f224
timotiviert <hofmann.ti@t-online.de>
2026-03-03 13:54:43 +0100

    Test Config in main, works :)

--- 03-rest-api/cmd/api/main.go
+++ 03-rest-api/cmd/api/main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net/http"
+	//"net/http"
+	//
+	//"github.com/gin-gonic/gin"
 
-	"github.com/gin-gonic/gin"
+	"github.com/timotiviert/go-learning/03-rest-api/internal/config"
 )
 
 func main() {
-	r := gin.Default()
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-
-	if err := r.Run(); err != nil {
-		log.Fatalf("failed to start server: %v", err)
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
 	}
+	fmt.Println(cfg.DbConnString)
+
+	//r := gin.Default()
+	//
+	//r.GET("/ping", func(c *gin.Context) {
+	//	c.JSON(http.StatusOK, gin.H{
+	//		"message": "pong",
+	//	})
+	//})
+	//
+	//if err := r.Run(); err != nil {
+	//	log.Fatalf("failed to start server: %v", err)
+	//}
 }
